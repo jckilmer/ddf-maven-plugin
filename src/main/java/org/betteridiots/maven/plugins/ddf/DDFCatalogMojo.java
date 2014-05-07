51851c61fa317e39e05ff12dcf736c44e6664277
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2014-05-06 22:55:46 -0400

    DMP-4 DMP-5: Added support for catalog:ingest and catalog:removeall

---
+++ src/main/java/org/betteridiots/maven/plugins/ddf/DDFCatalogMojo.java
@@ -0,0 +1,105 @@
+package org.betteridiots.maven.plugins.ddf;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.betteridiots.ssh.SshExecFactory;
+
+
+@Mojo( name="catalog-commands", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST )
+public class DDFCatalogMojo extends AbstractMojo {
+    /**
+     * File containing DDF commands (optional)
+     */
+    @Parameter( property = "config-ddf.paramsFile")
+    private String paramsFile;
+
+    /**
+     * Username credentials for accessing ddf
+     */
+    @Parameter( property = "config-ddf.user", defaultValue = "admin" )
+    private String user;
+
+    /**
+     * Password for accessing the ddf
+     */
+    @Parameter( property = "config-ddf.password", defaultValue = "admin" )
+    private String password;
+
+    /**
+     * Hostname or IP address of the ddf
+     */
+    @Parameter( property = "config-ddf.host", defaultValue = "localhost" )
+    private String host;
+
+    /**
+     * SSH Port for the ddf
+     */
+    @Parameter( property = "config-ddf.port", defaultValue = "8101" )
+    private int port;
+
+    /**
+     * Operation to be executed
+     */
+    @Parameter( property = "catalog.operation", required = true)
+    private String operation;
+
+    /**
+     * Transformer to use for ingest
+     */
+    @Parameter( property = "catalog.transformer")
+    private String transformer;
+
+    /**
+     * @deprecated
+     * Use provider
+     */
+    @Parameter( property = "catalog.provider")
+    private boolean provider;
+
+    /**
+     * Path to metacards for ingest
+     */
+    @Parameter( property = "catalog.data")
+    private String data;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        String sshCommand = null;
+
+        // Log Parameters
+        getLog().info( "DDF Host is: " + host );
+        getLog().info( "DDF Port is: " + port );
+        getLog().info( "DDF user is: " + user );
+        getLog().info( "DDF password is: " + password );
+
+        if (operation.equals("ingest")) {
+            if (transformer == null)
+                getLog().error("Transformer not specified, this is required for performing ingest");
+            else
+                sshCommand = "catalog:" + operation + " -p -t " + transformer + " " + data;
+        }
+        if (operation.equals("removeall")) {
+            sshCommand = "catalog:" + operation + " -f -p";
+        }
+        else
+            getLog().error( "The requested operation: " + operation + " is not supported yet, or it does not exist");
+
+        // Disable strict host key checking and set auth method to password
+        java.util.Properties sshConfig = new java.util.Properties();
+        sshConfig.put("StrictHostKeyChecking", "no");
+        sshConfig.put("PreferredAuthentications","password");
+
+        // Build JSch Session
+
+        SshExecFactory sef = new SshExecFactory();
+
+        getLog().info("Executing operation: " + sshCommand);
+        sef.buildChannel(user, password, host, port, sshConfig, sshCommand);
+    }
+}
